38ed12840ebfe010e00e4ea50c807ab5dd699eb4
jeronimovelasqz <103902591+jeronimovelasqz@users.noreply.github.com>
2023-08-02 15:04:36 -0500

    Update ejercicio.py

--- Ejercicios de estructuras/ejercicio.py
+++ Ejercicios de estructuras/ejercicio.py
@@ -1,13 +1,15 @@
-def calcular_longitud(longitud: str, contador: int = 0):
+def calcular_longitud(longitud: str, contador: int = 0, vacio = ""):
 
     if not longitud:
-        return
+        print(vacio)
+        return vacio
 
     else:
         contador += 1
-        print(longitud[-1])
-        return calcular_longitud(longitud[:-1], contador)
+        #print(longitud[-1])
+        vacio = vacio + longitud[-1]
+        return calcular_longitud(longitud[:-1], contador, vacio)
 
 
 
-calcular_longitud("HOLA")
+(calcular_longitud("juan david"))
