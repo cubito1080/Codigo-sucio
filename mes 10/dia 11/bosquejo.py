bb77dc01021b2938d8713c90b94d89a9ba60d993
jeronimovelasqz <103902591+jeronimovelasqz@users.noreply.github.com>
2023-08-12 14:15:09 -0500

    Update Complejidad.md

--- mes 10/dia 11/bosquejo.py
+++ mes 10/dia 11/bosquejo.py
@@ -10,12 +10,15 @@ class Matriz:
         else:
             raise ValueError("La matriz debe ser cuadrada e impar")
 
-
+        # revisar miro
         self.cuantas_derecha = math.ceil(self.filas/2)
 
-        self.cuantas_arriba = math.floor(self.filas/2)
-        self.cuantas_izquierda = math.floor(self.filas / 2)
-        self.cuantas_izquierda = math.floor(self.filas / 2)
+        # revisar miro , patron matematico
+        """self.cuantas_arriba = math.floor(self.filas/2)
+        self.cuantas_abajo = math.floor(self.filas / 2)
+        self.cuantas_izquierda = math.floor(self.filas / 2)"""
+
+        self.almacenar_impresion = []
 
 
     def agregar_numero(self, fila_objetivo: int, columna_objetivo: int, agregar_elemento: int):
@@ -25,25 +28,17 @@ class Matriz:
         else:
             return False
 
-    def calcular_centro(self):
-        centro_fila = self.filas // 2
-        centro_columna = self.columnas // 2
-        return centro_fila, centro_columna
+    def calcular_centro_fila(self):
+        centro_fila = math.ceil(self.filas / 2)
+        return centro_fila
+
+    def calcular_centro_columna(self):
+        centro_columna = math.ceil(self.filas / 2)
+        return centro_columna
 
     def calcular_rotaciones_espirales(self):
         return self.filas // 2
 
-    def girar_derecha(self, centro: list, numero_capa):
-        centro = [self.filas][self.columnas + 1]
-
-    def girar_izquierda(self, centro: list, numero_capa):
-        centro = [self.filas][self.columnas - 1]
-
-    def girar_arriba(self, centro: list, numero_capa):
-        centro = [self.filas - 1][self.columnas]
-
-    def girar_abajo(self, centro: list, numero):
-        centro = [self.filas + 1][self.columnas]
 
     def hallar_serie_izquierda_y_arriba(self):
         serie_izquierda_y_arriba = []
@@ -55,7 +50,6 @@ class Matriz:
 
         return serie_izquierda_y_arriba
 
-
     def hallar_serie_derecha_abajo(self):
         serie_derecha_abajo = []
         contador = 1
@@ -65,6 +59,60 @@ class Matriz:
             contador += 2
 
         return serie_derecha_abajo
+
+    def imprimir_caracola(self):
+
+        # calcular centro incial
+
+        centro_fila = self.calcular_centro_fila()
+        centro_columna = self.calcular_centro_columna()
+
+        # hallar series
+
+        serie_derecha:list = self.hallar_serie_derecha_abajo()
+        serie_abajo:list = self.hallar_serie_derecha_abajo()
+
+        serie_izquierda:list = self.hallar_serie_izquierda_y_arriba()
+        serie_arriba:list = self.hallar_serie_izquierda_y_arriba()
+
+        # patron de impresion
+        #primero derecha
+        # segundo abajo
+        #tercero izquierda
+        #cuarto arriba
+
+        #repetir
+
+        while self.cuantas_derecha >= 0:
+
+            # hacer derecha primero
+            for girar_derechas in range(serie_derecha[0]):
+                self.almacenar_impresion.append(self.lista_columnas[centro_fila][centro_columna + 1])
+                centro_columna = centro_columna + 1
+
+            serie_derecha.pop(0)
+
+            # luego hacer abajos
+            for girar_abajos in range(serie_abajo[0]):
+                self.almacenar_impresion.append(self.lista_columnas[centro_fila + 1][centro_columna])
+                centro_fila = centro_fila + 1
+
+
+            #luego hacer izquierda
+            for girar_izquierda in range(serie_izquierda[0]):
+                self.almacenar_impresion.append(self.lista_columnas[centro_fila][centro_columna - 1])
+                centro_columna = centro_columna - 1
+
+            # finalmente girar arribas
+            for girar_arribas in range(serie_arriba[0]):
+                self.almacenar_impresion.append(self.lista_columnas[centro_fila - 1][centro_columna])
+                centro_fila = centro_fila - 1
+
+
+
+
+
+
 
 
 
